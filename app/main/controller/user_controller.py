2e2038d0739905d65f37fa2060ff1aea6a0801c9
FADIcheikh <fadi.cheikh@esprit.tn>
2018-10-22 15:57:16 +0200

    2nd commit

--- app/main/controller/user_controller.py
+++ app/main/controller/user_controller.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_restplus import Resource
 
 from ..util.userDTO import UserDTO
-from ..service.user_service import  get_all_users
+from ..service.user_service import  get_all_users,get_a_user
 
 api = UserDTO.api
 _user = UserDTO.user
@@ -14,3 +14,13 @@ class UserList(Resource):
     @api.marshal_list_with(_user, envelope='data')
     def get(self):
         return get_all_users()
+
+"""GET BY ID"""
+
+@api.route('/<_id>')
+@api.param('_id', 'User identifier')
+class User(Resource):
+    @api.doc('get_user_by_id')
+    @api.marshal_with(_user, envelope='data')
+    def get(self,_id):
+        return get_a_user(_id)
--- app/main/service/user_service.py
+++ app/main/service/user_service.py
@@ -7,3 +7,5 @@ def get_all_users():
     return User.query.all()
 
 
+def get_a_user(_id):
+    return User.query.filter_by(id=_id).first()
