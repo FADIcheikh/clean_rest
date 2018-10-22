bc3463dd052ad6faf8304c554f8591f6a8be6d68
FADIcheikh <fadi.cheikh@esprit.tn>
2018-10-23 01:16:57 +0200

    5th commit

--- app/main/controller/user_controller.py
+++ app/main/controller/user_controller.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_restplus import Resource
 
 from ..util.userDTO import UserDTO
-from ..service.user_service import  get_all_users,get_a_user,add_a_user
+from ..service.user_service import  get_all_users,get_a_user,add_a_user,update_a_user
 
 
 api = UserDTO.api
@@ -22,10 +22,21 @@ class UserList(Resource):
     @api.expect(_user, 'new user')
     @api.doc('Add a user')
     @api.response(201, 'User created.')
+    @api.response(400, 'email already exists.')
     def post(self):
         data = request.json
         return add_a_user(data=data)
 
+
+    """PUT"""
+
+    @api.expect(_user, 'user to update')
+    @api.doc('update a user')
+    @api.response(200, 'User updated.')
+    @api.response(201, 'User doesnt exists, the given user is created instead.')
+    def put(self):
+        data = request.json
+        return update_a_user(data=data)
 
 
 """GET BY ID"""
--- app/main/service/user_service.py
+++ app/main/service/user_service.py
@@ -30,7 +30,6 @@ def get_a_user(_id):
 
 def add_a_user(data):
     user = User.query.filter_by(email=data['email']).first()
-    print user
     if not user:
         user_to_add = User(
             email=data['email'],
@@ -38,21 +37,50 @@ def add_a_user(data):
             password=data['password']
         )
         commit(user_to_add)
-        response_object = {
-            'status': 'fail',
-            'message': 'there is already a user with the given email.',
-        }
-        return response_object, 400
-    else:
+
         response_object = {
             'status': 'success',
             'message': 'user created.',
         }
         return response_object,201
+    else:
+        response_object = {
+            'status': 'fail',
+            'message': 'there is already a user with the given email.',
+        }
+        return response_object, 400
+
+
+def update_a_user(data):
+    user = User.query.filter_by(email=data['email']).first()
+    if not user:
+        response_object = {
+            'status': 'fail',
+            'message': 'there is no user with the given email, user created instead.',
+        }
+        return response_object, 201
+
+    else:
+
+
+        user.email = data['email']
+        user.password=data['password']
+        user.nom=data['nom']
+        merge(user)
+
+        response_object = {
+            'status': 'success',
+            'message': 'user updated.',
+        }
+        return response_object,200
 
 
 
 
 def commit(data):
     db.session.add(data)
+    db.session.commit()
+
+def merge(data):
+    db.session.merge(data)
     db.session.commit()
